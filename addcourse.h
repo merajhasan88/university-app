2b6715f153130606acb05fc77ec0018afc155e81
Meraj Hasan <meraj.hasan.1988@gmail.com>
2024-04-28 23:17:12 +0500

    Added dynamic image button

--- addcourse.h
+++ addcourse.h
@@ -7,7 +7,7 @@
 #include <QTableView>
 #include <QItemDelegate>
 #include <QGraphicsScene>
-
+#include <qpushbutton.h>
 namespace Ui {
 class AddCourse;
 }
@@ -17,6 +17,7 @@ class AddCourse : public QMainWindow
     Q_OBJECT
 
 public:
+    QPushButton *push_buttonImage = new QPushButton(this);
     QString table_name="";
     QString id = "";
     QString name = "";
@@ -26,18 +27,21 @@ public:
     QByteArray image_bytes;
     int col = 0;
        int row = 0;
-       int sizex = 5;
+       int sizex = 6;
        int sizey = 0;
        QGraphicsScene *scene = new QGraphicsScene(this);
     explicit AddCourse(QWidget *parent = nullptr);
     ~AddCourse();
 signals:
        void on_closeButtonClicked();
+    void onImageCellClicked();
 public slots:
        void closeEvent(QCloseEvent *event);
+    void openImage();
 private slots:
     void on_submitButton_clicked();
     void addColToTableView(QString,int);
+    void addImageColToTableView(int);
         //void addRowToTableView(QString);
 void showEvent(QShowEvent *event);
         void setTitle();
